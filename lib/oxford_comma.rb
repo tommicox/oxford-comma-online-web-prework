5261fae7ab39a30e065d8ced8601de295c166bab
Tommi Cox <tommicox@gmail.com>
2019-03-04 21:44:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+    elsif 2 < array.length
+    array[-1].insert(0, "and ")
+     end
+ return array.join(", ")
 end
+
+
+
+
+
+  
