d2f582645641edbdc267660b9ba175f067048476
Tommi Cox <tommicox@gmail.com>
2019-03-04 21:40:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
     elsif 2 < array.length
-    array.insert(1, "and")
+    array.insert(4, "and")
     
   
   end
