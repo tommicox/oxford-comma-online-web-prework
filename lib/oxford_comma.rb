a709f9cc61795d911ead53c1001b08ef70a7ae55
Tommi Cox <tommicox@gmail.com>
2019-03-04 21:31:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
     elsif 2 < array.length
     array.insert(4, "and")
-    
+    return array.join
   
   end
  return array.join(",")
